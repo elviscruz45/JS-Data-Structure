e986a32e8a98fd05a873f47566eb512bcbb4f2f6
Elvis <elviscruz45@gmail.com>
2022-09-23 22:13:10 -0500

    finished

---
+++ hashTable/hashtable.js
@@ -0,0 +1,39 @@
+class HashTable {
+    constructor(size) {
+      this.data = new Array(size);
+    }
+    hashMethod(key) {
+      let hash = 0;
+      for (let i = 0; i < key.length; i++) {
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+      }
+      return hash;
+    }
+    set(key, value) {
+      const address = this.hashMethod(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
+  }
+  
+const myHashTable = new HashTable(50);
+
+
+
+console.log(myHashTable.set("Diego",1990))
+
+console.log(myHashTable.set("Mariana",1998))
+
+
+
+
+console.log(myHashTable.set("Alejandra",2000))
+
+
+
+
+
+
